647791a9fa36c62e3685159aba0f929973d07fdb
alireza kargar <imanesche@gmail.com>
2022-07-31 14:27:57 +0430

    add: enum movement state

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,10 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer sprite;
     private Rigidbody2D rb;
     private Animator anim;
+    float dirX = 0f;
+
+    private enum MovementState { stay, walking, jumping }
+    // private MovementState state = MovementState.stay;
 
     // Start is called before the first frame update
     void Start()
@@ -19,29 +23,43 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float dirX = Input.GetAxis("Horizontal");
+        dirX = Input.GetAxis("Horizontal");
         // setup movement
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
-        
+
         // Debug.Log(dirX);
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        MovementState state;
+
         if (Input.GetKey("space"))
         {
-           rb.velocity = new Vector2(rb.velocity.x, 7f);
+            rb.velocity = new Vector2(rb.velocity.x, 7f);
         }
 
         if (dirX > 0f)
         {
-            anim.SetBool("walking", true);
+            state = MovementState.walking;
             sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
-            anim.SetBool("walking", true);
+            state = MovementState.walking;
             sprite.flipX = true;
         }
         else
         {
-            anim.SetBool("walking", false);
+            state = MovementState.stay;
         }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+
+        anim.SetInteger("state", (int)state);
     }
 }
