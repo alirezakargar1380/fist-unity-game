bd0cfe2a17abdca7e35741b1f7946a72e073cab8
alireza kargar <imanesche@gmail.com>
2022-07-31 11:33:27 +0430

    add: flipX to PlayerMovement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private SpriteRenderer sprite;
     private Rigidbody2D rb;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,11 +22,26 @@ public class PlayerMovement : MonoBehaviour
         float dirX = Input.GetAxis("Horizontal");
         // setup movement
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
-
+        
         // Debug.Log(dirX);
         if (Input.GetKey("space"))
         {
-           rb.velocity = new Vector2(rb.velocity.x, 14f);
+           rb.velocity = new Vector2(rb.velocity.x, 7f);
+        }
+
+        if (dirX > 0f)
+        {
+            anim.SetBool("walking", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("walking", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("walking", false);
         }
     }
 }
